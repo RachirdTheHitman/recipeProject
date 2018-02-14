a8411002ec3b4b1b90615bbe7ec0135f6820383b
Richard <hyacinthzy1@gmail.com>
2018-02-14 12:52:17 +1100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { AppServerComponent } from './app-server/app-server.component';
+import {HeaderComponent} from './header/header.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppServerComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
